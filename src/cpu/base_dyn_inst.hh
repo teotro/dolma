37c78bd1723b73dfc2e8fd68392058242ce702d8
Theodoros Trochatos <70296021+teotro@users.noreply.github.com>
2021-03-25 20:47:59 +0200

    Update base_dyn_inst.hh

--- src/cpu/base_dyn_inst.hh
+++ src/cpu/base_dyn_inst.hh
@@ -177,6 +177,8 @@ class BaseDynInst : public ExecContext, public RefCounted
 
     /** InstRecord that tracks this instructions. */
     Trace::InstRecord *traceData;
+    
+    WaitClass latency   //[Teo] *TODO* Implement WaitClass... Probably at src/arch/x86/regs/misc.hh (?)
 
   protected:
     /** The result of the instruction; assumes an instruction can have many
